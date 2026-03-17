0bfc4c8f633bfb41e4a65d049944d1fe7dc2fb4e
GreyWorld <946596029@qq.com>
2026-03-17 17:14:58 +0800

    feat(dws): add new datasource for query the list of upgrade records of the cluster (#9120)
    Co-authored-by: greyworld <huawei@ecs-yanghongyu>

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2421,6 +2421,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_dws_cluster_parameters":              dws.DataSourceClusterParameters(),
 			"huaweicloud_dws_cluster_snapshot_statistics":     dws.DataSourceDwsClusterSnapshotStatistics(),
 			"huaweicloud_dws_cluster_topo_rings":              dws.DataSourceDwsClusterTopoRings(),
+			"huaweicloud_dws_cluster_upgrade_records":         dws.DataSourceClusterUpgradeRecords(),
 			"huaweicloud_dws_cluster_user_authorities":        dws.DataSourceClusterUserAuthorities(),
 			"huaweicloud_dws_clusters":                        dws.DataSourceDwsClusters(),
 			"huaweicloud_dws_disaster_recovery_tasks":         dws.DataSourceDisasterRecoveryTasks(),
---
+++ huaweicloud/services/acceptance/dws/data_source_huaweicloud_dws_cluster_upgrade_records_test.go
@@ -0,0 +1,48 @@
+package dws
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataClusterUpgradeRecords_basic(t *testing.T) {
+	all := "data.huaweicloud_dws_cluster_upgrade_records.test"
+	dc := acceptance.InitDataSourceCheck(all)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+			acceptance.TestAccPreCheckDwsClusterId(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataClusterUpgradeRecords_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestMatchResourceAttr(all, "records.#", regexp.MustCompile(`^[0-9]([0-9]*)?$`)),
+					resource.TestCheckResourceAttrSet(all, "records.0.id"),
+					resource.TestCheckResourceAttrSet(all, "records.0.status"),
+					resource.TestCheckResourceAttrSet(all, "records.0.record_type"),
+					resource.TestCheckResourceAttrSet(all, "records.0.from_version"),
+					resource.TestCheckResourceAttrSet(all, "records.0.to_version"),
+					resource.TestCheckResourceAttrSet(all, "records.0.start_time"),
+					resource.TestCheckResourceAttrSet(all, "records.0.job_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataClusterUpgradeRecords_basic() string {
+	return fmt.Sprintf(`
+data "huaweicloud_dws_cluster_upgrade_records" "test" {
+  cluster_id = "%[1]s"
+}
+`, acceptance.HW_DWS_CLUSTER_ID)
+}
